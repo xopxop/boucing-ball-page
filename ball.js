7cfb406787de818a6a0266713f663edd7b96309f
Du Than <du.than@relexsolutions.com>
2024-12-18 11:28:26 +0200

    saved

--- ball.js
+++ ball.js
@@ -4,9 +4,18 @@ class Ball {
     this.radius = radius;
     this.speed = speed;
     this.offset = 0;
+    this.direction = 1;
     this.center = this.track.getPosition(this.offset);
   }
- 
+
+  move() {
+    this.offset += this.speed * this.direction;
+    this.center = this.track.getPosition(this.offset);
+    if (this.center.y > this.track.center.y) {
+      this.direction *= -1;
+    }
+  }
+
   draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
--- track.js
+++ track.js
@@ -11,10 +11,14 @@ class Track {
     };
   }
 
-
   draw(ctx) {
     ctx.beginPath();
-    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+
+    for (let a = 0; a < Math.PI * 2; a+= 0.01) {
+      const pos = this.getPosition(a);
+      ctx.lineTo(pos.x, pos.y);
+    }
+    ctx.closePath();
     ctx.strokeStyle = "white";
     ctx.stroke();
   }
